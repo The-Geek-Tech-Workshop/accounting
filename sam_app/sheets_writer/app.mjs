c43b6adb9fe359773da87fd2e4680f79fdc586b9
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2024-07-11 16:22:55 +0000

    Calls written, can't authenticate

--- sam_app/sheets_writer/app.mjs
+++ sam_app/sheets_writer/app.mjs
@@ -1,3 +1,12 @@
+import google from "@googleapis/sheets";
+
+const SCOPES = [
+  "https://www.googleapis.com/auth/spreadsheets",
+  "https://www.googleapis.com/auth/drive.file",
+];
+
+const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -16,6 +25,48 @@ export const lambdaHandler = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({}),
   };
+
+  const sheetsApi = google.sheets({ version: "v4" });
+
+  const creditedAccount = "GTW";
+  const debitedAccount = "Equipment";
+  const accountTypeFunction = "";
+  const skuOrPurchaseId = "";
+
+  console.log(`SS-ID: ${SPREADSHEET_ID}`);
+
+  // Insert new row and then sort all rows by date column
+  const result = await sheetsApi.spreadsheets.batchUpdate({
+    spreadsheetId: SPREADSHEET_ID,
+    resource: [
+      {
+        insertRange: {
+          range: "Transactions!A3:I3",
+          shiftDimension: "ROWS",
+        },
+      },
+      {
+        updateCells: {
+          rows: [
+            {
+              rows: [
+                event.transactionDate,
+                creditedAccount,
+                debitedAccount,
+                accountTypeFunction,
+                event.amount,
+                skuOrPurchaseId,
+                event.description,
+                event.who,
+              ],
+            },
+          ],
+          fields: "*",
+          range: "Transactions!A3:I3",
+        },
+      },
+    ],
+  });
 
   return response;
 };
