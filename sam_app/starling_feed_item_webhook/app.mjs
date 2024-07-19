31b86b10aebf24a858e9aa9295cadd2cdc726a54
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2024-07-19 10:15:46 +0100

    Process starling events (#4)
    * Clean up sheets writer
    
    * Basic Starling done
    
    * use refernce as description
    
    * remove redundent test keys

--- sam_app/starling_feed_item_webhook/app.mjs
+++ sam_app/starling_feed_item_webhook/app.mjs
@@ -1,9 +1,11 @@
 import { createPublicKey, verify } from "crypto";
+import AWS from "aws-sdk";
 
 const PUBLIC_KEY = createPublicKey(
   `-----BEGIN PUBLIC KEY-----\n${process.env.PUBLIC_KEY}\n-----END PUBLIC KEY-----`
 );
 const ENCRYPTION_ALGORITHM = "RSA-SHA512";
+const QUEUE_URL = process.env.QUEUE_URL;
 
 /**
  *
@@ -18,17 +20,22 @@ const ENCRYPTION_ALGORITHM = "RSA-SHA512";
  *
  */
 
-export const lambdaHandler = async (event, context) => {
+export const lambdaHandler = async (event) => {
   const verified = verifyEvent(event);
 
-  console.log(`EVENT: ${JSON.stringify(event)}`);
+  if (verified) {
+    const sqs = new AWS.SQS();
+    await sqs
+      .sendMessage({
+        MessageBody: event.body,
+        QueueUrl: QUEUE_URL,
+      })
+      .promise();
+  }
 
-  const response = verified
+  return verified
     ? {
-        statusCode: 200,
-        body: JSON.stringify({
-          feedItem: event.body,
-        }),
+        statusCode: 202,
       }
     : {
         statusCode: 400,
@@ -36,8 +43,6 @@ export const lambdaHandler = async (event, context) => {
           error: "Integrity of message signature could not be verified",
         }),
       };
-
-  return response;
 };
 
 const verifyEvent = (event) => {
