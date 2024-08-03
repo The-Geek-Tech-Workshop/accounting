ff992a08d27b81974b4f27815e0268a745a9ddee
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2024-08-03 16:57:20 +0100

    Add starling business webhook (#11)

--- sam_app/starling_feed_item_webhook/app.mjs
+++ sam_app/starling_feed_item_webhook/app.mjs
@@ -7,19 +7,6 @@ const PUBLIC_KEY = createPublicKey(
 const ENCRYPTION_ALGORITHM = "RSA-SHA512";
 const QUEUE_URL = process.env.QUEUE_URL;
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-
 export const lambdaHandler = async (event) => {
   console.log(JSON.stringify(event));
 
