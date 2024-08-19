9d0e868ea052fa879d868d0918141a47313109ee
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2024-08-19 19:36:17 +0100

    Detect Capital and Drawings transactions (#17)

--- .devcontainer/create_auth_files.sh
+++ .devcontainer/create_auth_files.sh
@@ -4,3 +4,4 @@ echo $GOOGLE_SA_JSON > /workspaces/accounting/sam_app/sheets_writer/google_crede
 echo $EBAY_AUTH > /workspaces/accounting/sam_app/ebay_order_enricher/ebay-auth.json
 echo $EBAY_AUTH > /workspaces/accounting/sam_app/ebay_payout_enricher/ebay-auth.json
 echo $EBAY_AUTH > /workspaces/accounting/sam_app/ebay_token_gen/ebay-auth.json
+echo $PERSONAL_DATA > /workspaces/accounting/sam_app/starling_feed_item_to_transaction/personal.json
