5711b62d238822828ef9e4c265d26105df2aa816
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2024-10-29 12:50:05 +0000

    ebay cli tool factored out to own repo/docker

--- .devcontainer/create_auth_files.sh
+++ .devcontainer/create_auth_files.sh
@@ -4,7 +4,7 @@ echo $GOOGLE_SA_JSON > /workspaces/accounting/sam_app/sheets/transaction_writer/
 echo $GOOGLE_SA_JSON > /workspaces/accounting/sam_app/sheets/notification_writer/google_credentials.json
 echo $PERSONAL_DATA > /workspaces/accounting/sam_app/starling/feed_item_to_transaction/personal.json
 
-for dir in order_enricher api_caller token_gen transaction_fetcher non_sale_charge_to_transaction shipping_label_to_transaction sale_to_transaction sale_to_transaction
+for dir in order_enricher token_gen transaction_fetcher non_sale_charge_to_transaction shipping_label_to_transaction sale_to_transaction sale_to_transaction
 do
     echo $EBAY_AUTH > /workspaces/accounting/sam_app/ebay/$dir/ebay-auth.json
 done
