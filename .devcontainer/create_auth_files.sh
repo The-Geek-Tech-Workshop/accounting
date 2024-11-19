3e9fd0e8ce757393ed5ad62e4b424df51d5a35da
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2024-11-19 09:41:22 +0000

    Add config volume

--- .devcontainer/create_auth_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo $PERSONAL_DATA > /workspaces/accounting/sam_app/starling/feed_item_to_transaction/personal.json
-
-for dir in order_enricher token_gen transaction_fetcher non_sale_charge_to_transaction shipping_label_to_transaction sale_to_transaction sale_to_transaction refund_to_transaction
-do
-    echo $EBAY_AUTH > /workspaces/accounting/sam_app/ebay/$dir/ebay-auth.json
-done
